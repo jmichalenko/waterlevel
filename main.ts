1755856008d3ffed078408a9b55c7134945d6e6a
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2023-04-25 15:07:49 -0400

    code completed

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("reset")
+    basic.clearScreen()
+    min_level = 0
+    water_level = 0
+})
+let min_level = 0
+let water_level = 0
+basic.clearScreen()
+water_level = 0
+min_level = 0
 basic.forever(function () {
-	
+    if (input.pinIsPressed(TouchPin.P0)) {
+        water_level = 1
+        if (input.pinIsPressed(TouchPin.P0) && input.pinIsPressed(TouchPin.P1)) {
+            water_level = 3
+        }
+        if (input.pinIsPressed(TouchPin.P0) && (input.pinIsPressed(TouchPin.P1) && input.pinIsPressed(TouchPin.P2))) {
+            min_level = 6
+        }
+        if (water_level > min_level) {
+            min_level = water_level
+        }
+        basic.showString("" + (min_level))
+        basic.showString("cm")
+    }
 })
